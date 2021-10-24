39c5d457741ec8f0c30f7d13270c9ebd770076bf
Android789515 <derflug@mailfence.com>
2021-10-23 19:44:28 -0500

    Added curved corners to library panel.
    Implementing a gradient background for the media component.

--- src/components/library-components/Directory.jsx
+++ src/components/library-components/Directory.jsx
@@ -5,6 +5,11 @@ import '../../css/library-styles/library-directory-styles.scss'
 const Directory = props => {
     return (
         <div className={`directory ${props.name}-directory`}>
+            {/*<div className='directory-entries-area'>*/}
+            {/*    <ul className='non-default-ul directory-entries'>*/}
+            {/*        {props.entries}*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
             <ul className='non-default-ul directory-entries'>
                 {props.entries}
             </ul>
