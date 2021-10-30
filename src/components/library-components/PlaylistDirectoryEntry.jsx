494472ca9984079457efa22e0e45d8a7f353bfd1
Android789515 <derflug@mailfence.com>
2021-10-29 20:10:56 -0500

    Uncapped max width on library.
    Added rename button to playlist directory entry.
    
    Fixed bug where class list would be undefined in toggleVolumeBarHidden method on VolumeBtn.jsx.

--- src/components/library-components/PlaylistDirectoryEntry.jsx
+++ src/components/library-components/PlaylistDirectoryEntry.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { directories } from './DirectoryList'
 import { setOpenedPlaylist } from '../../reducers/librarySlice'
+import renamePlaylistBtn from '../../img/dark-btns/rename.svg'
 
 import DirectoryEntry from './DirectoryEntry'
 import SongDirectoryEntry from './SongDirectoryEntry'
@@ -20,6 +21,7 @@ const PlaylistDirectoryEntry = props => {
         >
             <h4 className='playlist-entry-title'>{props.playlist.name}</h4>
             <p className='playlist-entry-song-count'>{props.playlist.songs.length} songs</p>
+            <img className='rename-playlist-btn overlay-component' src={renamePlaylistBtn} alt='Rename playlist button' />
         </DirectoryEntry>
     )
 }
