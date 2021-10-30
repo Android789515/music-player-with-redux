dcea425abde7f92e3b8390dba44f2b22013613ce
Android789515 <derflug@mailfence.com>
2021-10-29 20:45:02 -0500

    Changed rename playlist button to show only when mouse is hovered over the playlist entry.

--- src/components/library-components/PlaylistDirectoryEntry.jsx
+++ src/components/library-components/PlaylistDirectoryEntry.jsx
@@ -20,20 +20,26 @@ const PlaylistDirectoryEntry = props => {
         return isNameTooLong ? cutName + '...' : name
     }
 
+    const openPlaylist = () => {
+        // Show songs in the playlist
+        props.dispatch(setOpenedPlaylist(props.playlist))
+
+        props.setCurrentDirectory(() => directories.openedPlaylist)
+    }
+
     return (
         <DirectoryEntry
             entry={props.playlist}
             className='btn directory-entry'
-            onClick={() => {
-                // Show songs in the playlist
-                props.dispatch(setOpenedPlaylist(props.playlist))
-
-                props.setCurrentDirectory(() => directories.openedPlaylist)
-            }}
+            onClick={openPlaylist}
         >
             <h4 className='playlist-entry-title'>{cutPlaylistName()}</h4>
             <p className='playlist-entry-song-count'>{props.playlist.songs.length} songs</p>
-            <img className='rename-playlist-btn overlay-component' src={renamePlaylistBtn} alt='Rename playlist button' />
+            <img
+                className='rename-playlist-btn overlay-component hidden'
+                src={renamePlaylistBtn}
+                alt='Rename playlist button'
+            />
         </DirectoryEntry>
     )
 }
