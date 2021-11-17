f263d7798d042ec138fab337046b7b23f0634783
Android789515 <derflug@mailfence.com>
2021-11-16 20:17:16 -0600

    Added MediaInfo scss module.

--- src/components/media-components/MediaInfo.jsx
+++ src/components/media-components/MediaInfo.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { softRoundedCorners, hidden } from '../../css/modules/GenericStyles.module.scss'
+import { mediaInfo, coverArt } from '../../css/modules/media-player/MediaInfo.scss'
+
 import SongInfoComponent from './SongInfoComponent'
 import fallBackImg from '../../img/cover-art-fallback.svg'
 
@@ -7,9 +10,9 @@ const MediaInfo = ({ queuedSong, media }) => {
     const isSongQueued = queuedSong.src !== undefined
 
     return (
-        <section className='media-info'>
+        <section className={mediaInfo}>
             <img
-                className={`cover-art softRoundedCorners ${isSongQueued ? '' : 'hidden'}`.trim()}
+                className={`${coverArt} ${softRoundedCorners} ${isSongQueued ? '' : hidden}`.trim()}
                 src={queuedSong.coverArt || fallBackImg}
                 alt='Song cover art'
             />
