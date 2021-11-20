9c85452d6dc7405284ef75a61994842b921f0153
Android789515 <derflug@mailfence.com>
2021-11-20 17:37:57 -0600

    Fixed LibraryBtn.module.scss.
    Added style module to ModalBtn component.

--- src/components/library-components/generic-components/ModalBtn.jsx
+++ src/components/library-components/generic-components/ModalBtn.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import { hardRoundedCorners } from '../../../css/modules/GenericStyles.module.scss'
+import { modalBtnBase, modalBtn, disabled } from '../../../css/modules/Modal.module.scss'
+
 const ModalBtn = ({ doOnClick, btnText, ...props }) => {
     return (
-        <button className='modal-btn-base hardRoundedCorners' {...props} >
+        <button className={`${modalBtnBase} ${hardRoundedCorners}`} {...props} >
             <span
-                className={`btn modal-btn hardRoundedCorners ${props.disabled ? 'disabled' : ''}`.trim()}
+                className={`btn ${modalBtn} ${hardRoundedCorners} ${props.disabled ? disabled : ''}`.trim()}
                 onClick={doOnClick}
                 onMouseEnter={props.onMouseEnter}
                 onMouseLeave={props.onMouseLeave}
