27f9b16f2a438448b290fdbcb0e9e70cb2c22f84
Android789515 <derflug@mailfence.com>
2021-11-21 11:24:05 -0600

    Installed TypeScript and refactored entryTypes to enum.

--- src/entryTypes.js
+++
@@ -1,4 +0,0 @@
-export default {
-    _SONGS: 'Songs',
-    _PLAYLISTS: 'Playlists'
-}
---
+++ src/entryTypes.ts
@@ -0,0 +1,6 @@
+enum EntryTypes {
+    _SONGS = 'Songs',
+    _PLAYLISTS = 'Playlists'
+}
+
+export default EntryTypes
