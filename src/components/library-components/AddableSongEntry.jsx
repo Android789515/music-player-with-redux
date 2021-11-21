3d72d1c5e450bdfce314934946c730ce6e7febde
Android789515 <derflug@mailfence.com>
2021-11-21 12:09:17 -0600

    Added style module for addable song entry component.

--- src/components/library-components/AddableSongEntry.jsx
+++ src/components/library-components/AddableSongEntry.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
+import { hardRoundedCorners } from '../../css/modules/GenericStyles.module.scss'
+import { addBtn } from '../../css/modules/buttons/AddBtn.module.scss'
+import { songEntryTitle, songEntryArtist } from '../../css/modules/library/SongDirectoryEntry.module.scss'
+import styles from '../../css/modules/library/AddableSongEntry.module.scss'
+
 import { getFormattedSongTime } from '../../TimeFormatter'
 import useButtonToggle from '../../hooks/useButtonToggle'
 
@@ -27,19 +32,26 @@ const AddableSongEntry = ({ updateSongsToAdd, song }) => {
     }
 
     const id = uuidv4()
+    const {
+        addableSong,
+        selected,
+        directoryEntry,
+        songEntryInfo,
+        songEntryDuration
+    } = styles
     return (
-        <div className={`addable-song hardRoundedCorners ${isToggled ? 'selected' : ''}`.trim()} key={id}>
-            <button className='btn entry-btn'>{isToggled ? '-' : '+'}</button>
+        <div className={`${addableSong} ${hardRoundedCorners} ${isToggled ? selected : ''}`.trim()} key={id}>
+            <button className={`btn ${addBtn}`}>{isToggled ? '-' : '+'}</button>
 
             <DirectoryEntry
                 entry={song}
-                className='btn directory-entry'
+                className={`btn ${directoryEntry}`}
                 id={song.id}
                 onClick={handleClick}
             >
-                <h4 className='song-entry-title song-entry-info'>{song.title}</h4>
-                <p className='song-entry-artist song-entry-info'>{song.artist}</p>
-                <p className='song-entry-duration'>{getFormattedSongTime(song.duration)}</p>
+                <h4 className={`${songEntryTitle} ${songEntryInfo}`}>{song.title}</h4>
+                <p className={`${songEntryArtist} ${songEntryInfo}`}>{song.artist}</p>
+                <p className={`${songEntryDuration}`}>{getFormattedSongTime(song.duration)}</p>
             </DirectoryEntry>
         </div>
     )
