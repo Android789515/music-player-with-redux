36aebfc6b427c734c49de3f79fe0b4dbf4262b95
Android789515 <derflug@mailfence.com>
2021-11-23 18:16:45 -0600

    Fixed styling for add/remove button in the addable song entry modal.

--- src/TODO.md
+++ src/TODO.md
@@ -4,6 +4,5 @@
 
 * ###Move btn class to its own module
 * ###Fix bug with context menu due to it being positioned fixed.
-* ###Fix styling for add button in the add to playlist modal
 * ###Remove button theme logic
 * ###Fix font and svg sizes at small screens
--- src/components/library-components/AddableSongEntry.jsx
+++ src/components/library-components/AddableSongEntry.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 import { hardRoundedCorners } from '../../css/GenericStyles.module.scss'
-import { addBtn } from '../../css/buttons/AddBtn.module.scss'
+import { addBtn, removeBtn, forAddableSong } from '../../css/buttons/AddBtn.module.scss'
 import { songEntryTitle, songEntryArtist } from '../../css/library/SongDirectoryEntry.module.scss'
 import styles from '../../css/library/AddableSongEntry.module.scss'
 
@@ -40,14 +40,21 @@ const AddableSongEntry = ({ updateSongsToAdd, song, isMarked = false }) => {
         songEntryDuration
     } = styles
     return (
-        <div className={`${addableSong} ${hardRoundedCorners} ${isToggled ? selected : ''}`.trim()} key={id}>
-            <button className={`btn ${addBtn}`}>{isToggled ? '-' : '+'}</button>
+        <div
+            className={`${addableSong} ${hardRoundedCorners} ${isToggled ? selected : ''}`.trim()}
+            key={id}
+            onClick={handleClick}
+        >
+            <button className={
+            `
+                btn ${isToggled ? removeBtn : addBtn} ${forAddableSong}
+            `
+            }>{isToggled ? '-' : '+'}</button>
 
             <DirectoryEntry
                 entry={song}
                 className={`btn ${directoryEntry}`}
                 id={song.id}
-                onClick={handleClick}
             >
                 <h4 className={`${songEntryTitle} ${songEntryInfo}`}>{song.title}</h4>
                 <p className={`${songEntryArtist} ${songEntryInfo}`}>{song.artist}</p>
