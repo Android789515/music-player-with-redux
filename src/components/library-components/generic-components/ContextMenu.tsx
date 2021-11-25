380b615a7c27396dfc7ce679812a6e18df6fddeb
Android789515 <derflug@mailfence.com>
2021-11-25 13:51:38 -0600

    Refactored ContextMenu component into TypeScript.

---
+++ src/components/library-components/generic-components/ContextMenu.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid'
+
+import genericStyles from '../../../css/GenericStyles.module.scss'
+import styles from '../../../css/library/ContextMenu.module.scss'
+import { setModalContent } from '../../../reducers/modalSlice'
+
+import PlaylistModal from '../../modals/PlaylistModal'
+import DeleteEntryModal from '../../modals/DeleteEntryModal'
+
+const capitalize = (string: string) => string.charAt(0).toUpperCase() + string.slice(1)
+
+export const contextMenuOptions = {
+    _queue: 'queue',
+    _open: 'open',
+    _rename: 'rename',
+    _delete: 'delete'
+}
+
+interface Props {
+    closeContextMenu: (event: React.FocusEvent) => void,
+    entry: {
+        id: number
+    },
+    entryRef: {
+        current: HTMLLIElement
+    },
+    shouldShow: boolean,
+    position: number,
+    contextoptions: string[]
+}
+
+const ContextMenu = ({ closeContextMenu, entry, entryRef, shouldShow, position, contextoptions }: Props) => {
+    const dispatch = useDispatch()
+
+    const handleContextOptionClick = (event: React.MouseEvent) => {
+        event.stopPropagation()
+
+        const { target } = event
+        const optionClicked = (target as HTMLLIElement).textContent?.toLowerCase()
+        const closestEntry = entryRef.current
+        closeContextMenu((event as unknown) as React.FocusEvent)
+
+        switch (optionClicked) {
+            case contextMenuOptions._queue:
+            case contextMenuOptions._open:
+                closestEntry.click()
+                break
+
+            case contextMenuOptions._rename:
+                if (closestEntry.firstElementChild) {
+                    const entryName = closestEntry.firstElementChild.textContent
+                    dispatch(setModalContent(<PlaylistModal
+                        action='rename'
+                        prevName={entryName}
+                        playlistId={entry.id}
+                    />))
+                }
+                break
+
+            case contextMenuOptions._delete:
+                dispatch(setModalContent(<DeleteEntryModal entry={entry} />))
+                break
+
+            default:
+                return
+        }
+    }
+
+    const handleLoseFocus = (event: React.FocusEvent) => {
+        const { target: contextMenu, relatedTarget } = event
+        if (contextMenu) {
+            const isContextOptionFocused = (contextMenu as HTMLUListElement).contains(relatedTarget as Node)
+
+            if (!isContextOptionFocused) {
+                closeContextMenu(event)
+            }
+        }
+    }
+
+    const { contextMenuOption } = styles
+    const contextMenuList = contextoptions.map(option => {
+        const uuid = uuidv4()
+        return (
+            <li
+                key={uuid}
+                className={contextMenuOption}
+                tabIndex={0}
+                onClick={handleContextOptionClick}
+            >
+                {capitalize(option)}
+            </li>
+        )
+    })
+
+    const contextMenuRef = useRef<HTMLUListElement>(null)
+
+    useEffect(() => {
+        if (shouldShow && contextMenuRef.current) {
+            contextMenuRef.current.focus()
+        }
+    }, [shouldShow])
+
+    const { contextMenu } = styles
+    if (shouldShow) {
+        const { hardRoundedCorners } = genericStyles
+        return (
+            <ul
+                className={`${contextMenu} ${hardRoundedCorners}`}
+                style={{ left: position }}
+                tabIndex={0}
+                ref={contextMenuRef}
+                onBlur={handleLoseFocus}
+            >
+                {contextMenuList}
+            </ul>
+        )
+    }
+
+    return <></>
+}
+
+export default ContextMenu
